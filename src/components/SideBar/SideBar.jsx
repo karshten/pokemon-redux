7d04afb9b68ddd486a69bf73c5c44109ca865aee
oskar shtefan <oskar.shtefan@gmail.com>
2022-09-16 20:50:56 +0600

    added border in side bar and pokeball

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -4,6 +4,7 @@ import './scss/sideBar.scss'
 export const SideBar = () => {
     return (
         <aside className='sideBar'>
+            <div className="pokeball"></div>
             <div className='sideBar__content'>
                 <h2>SideBar</h2>
             </div>
