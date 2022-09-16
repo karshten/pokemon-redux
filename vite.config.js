9d777e97a45dd1ed99ccf834fa228f2f470abbe2
oskar shtefan <oskar.shtefan@gmail.com>
2022-09-16 15:55:06 +0600

    added aliases for comfortable imports

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+
+  resolve: {
+    alias: [
+      { find: "@", replacement: path.resolve(__dirname, 'src') },
+      { find: "components", replacement: path.resolve(__dirname, 'src/components') },
+      { find: "hooks", replacement: path.resolve(__dirname, 'src/hooks') },
+      { find: "img", replacement: path.resolve(__dirname, 'src/assests/img') },
+      { find: "icons", replacement: path.resolve(__dirname, 'src/assets/icons') },
+      { find: "pages", replacement: path.resolve(__dirname, 'src/pages') },
+    ]
+  },
 })
