d02e230cbd7d0f9893787813fb548e452566ec60
oskar shtefan <oskar.shtefan@gmail.com>
2022-09-18 15:40:24 +0600

    added toolkit, store, features and pokemonSlice

--- src/pages/Pokemons/PokemonsList.jsx
+++ src/pages/Pokemons/PokemonsList.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import './scss/pokemons.scss'
 import { SortBy } from './SortBy'
 
 export const PokemonsList = () => {
+    const pokemons = useSelector(state => state.pokemon.pokemonsList)
+    console.log(pokemons);
     return (
         <main>
             <div className="main-container">
--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import pokemonsSlice from "../features/pokemons";
 
 export const store = configureStore({
     reducer: {
-        
+        pokemon: pokemonsSlice
     },
 })
