102a1e56a28cb860da357c89604cda97f126199c
oskar shtefan <oskar.shtefan@gmail.com>
2022-09-20 19:01:32 +0600

    added getting pokemon description

--- src/features/pokemon.jsx
+++ src/features/pokemon.jsx
@@ -9,10 +9,25 @@ const url = 'https://pokeapi.co/api/v2/pokemon';
 export const getPokemon = createAsyncThunk(
     'pokemon/getPokemon',
     async (payload, { rejectWithValue, dispatch }) => {
+
         const response = await fetch(`${url}/${payload}`)
         const data = await response.json()
 
         dispatch(setPokemon(data))
+        dispatch(getPokemonsDescription(data.species.url))
+    }
+)
+
+export const getPokemonsDescription = createAsyncThunk(
+    'pokemon/getPokemonsDescription',
+    async (payload, { rejectWithValue, dispatch }) => {
+
+        const descriptionRes = await fetch(payload)
+        const descriptionData = await descriptionRes.json()
+
+        const output = Array.from(new Set(descriptionData.flavor_text_entries.map(item => item.flavor_text))).join('')
+
+        dispatch(setPokemonDescription(output))
     }
 )
 
@@ -22,6 +37,9 @@ export const pokemonSlice = createSlice({
     reducers: {
         setPokemon: (state, action) => {
             state.pokemon = action.payload
+        },
+        setPokemonDescription: (state, action) => {
+            state.pokemon.description = action.payload
         }
     },
     extraReducers: {
@@ -31,5 +49,5 @@ export const pokemonSlice = createSlice({
     }
 })
 
-export const { setPokemon } = pokemonSlice.actions
+export const { setPokemon, setPokemonDescription } = pokemonSlice.actions
 export default pokemonSlice.reducer
