fe4efbb92b9befc4529ee1c64b62c7326added18
oskar shtefan <oskar.shtefan@gmail.com>
2022-09-20 16:44:33 +0600

    added middleware settings for prevent wrnings

--- src/store/store.js
+++ src/store/store.js
@@ -4,5 +4,9 @@ import pokemonsSlice from "../features/pokemons";
 export const store = configureStore({
     reducer: {
         pokemon: pokemonsSlice
-    }
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        immutableCheck: { warnAfter: 128 },
+        serializableCheck: { warnAfter: 128 },
+    })
 })
