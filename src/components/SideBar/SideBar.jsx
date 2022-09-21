0b3534ea36238d1e5b51e3f178892548e8b632e6
oskar shtefan <oskar.shtefan@gmail.com>
2022-09-21 21:16:48 +0600

    added generation radios and animation

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './scss/sideBar.scss'
+import arrow from 'icons/arrow.svg'
 
+const generations = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9
+]
 export const SideBar = () => {
+    const [genOpen, setGenOpen] = useState(true)
     return (
         <aside className='sideBar'>
             <div className="pokeball"></div>
             <div className='sideBar__content'>
-                <h2>SideBar</h2>
+                <div className="sideBar__generation">
+                    <div className="sideBar__toggle">
+                        <h3>Поколение</h3>
+                        <img
+                            onClick={() => setGenOpen(!genOpen)}
+                            className={!genOpen ? 'open' : 'closed'}
+                            src={arrow}
+                            alt="arrow" />
+                    </div>
+                    <div className="sideBar__radio-content" id={genOpen ? 'open' : 'closed'}>
+                        {generations.map(item => (
+                            <div key={item} className='sideBar__radio-gen'>
+                                <input type="radio" name="generation" value={item} />
+                                <label>
+                                    {item === generations.length ? item + '-e и новее' : item + '-e'}
+                                </label>
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </div>
         </aside>
     )
