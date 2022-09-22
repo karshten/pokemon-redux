6f636b2ccbe97f40022558aa9cea403be1a7e385
oskar shtefan <oskar.shtefan@gmail.com>
2022-09-22 23:18:10 +0600

    fixed bag with image when generation 5+

--- src/pages/Pokemons/PokemonItem.jsx
+++ src/pages/Pokemons/PokemonItem.jsx
@@ -5,7 +5,7 @@ export const PokemonItem = ({ pokemon }) => {
     return (
         <li className='pokemons__item'>
             <Link to={`/pokemons/:${pokemon.id}`}>
-                <img src={pokemon.sprites.other.dream_world.front_default}
+                <img src={pokemon.sprites.other.dream_world.front_default || pokemon.sprites.other.home.front_default}
                     alt={pokemon.name ? pokemon.name : 'pokemon name'} />
                 <h3>{pokemon.name}</h3>
             </Link>
