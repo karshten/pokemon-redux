7a2c96160eaf139f14fc4ba478f02b23cc43e015
oskar shtefan <oskar.shtefan@gmail.com>
2022-09-26 23:11:16 +0600

    divided into some scss fils in sidebar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import './scss/sideBar.scss'
+import './scss/generationRadioInput.scss'
+import './scss/pokemonType.scss'
 import arrow from 'icons/arrow.svg'
 import { useDispatch } from 'react-redux'
 import { getGeneration, setPokemons } from '../../features/Pokemons/pokemons'
@@ -9,12 +11,16 @@ const generations = [
 ]
 export const SideBar = () => {
     const [genOpen, setGenOpen] = useState(true)
+    const [typeOpen, setTypeOpen] = useState(true)
+    const [physicalType, setPhysicalType] = useState(false)
+    const [specialType, seSpecialType] = useState(false)
+
     const dispatch = useDispatch()
 
     const handleFilterByGeneration = (e) => {
         const generation = e.target.value
         dispatch(setPokemons([]))
-        dispatch(getGeneration({endPoint: `generation/${generation}`}))
+        dispatch(getGeneration({ endPoint: `generation/${generation}` }))
     }
 
     return (
@@ -30,7 +36,7 @@ export const SideBar = () => {
                             src={arrow}
                             alt="arrow" />
                     </div>
-                    <div className={genOpen ? 'openNav' : 'closedNav'}>
+                    {genOpen && <div className={genOpen ? 'openNav' : 'closedNav'}>
                         {generations.map(item => (
                             <div key={item} className='sideBar__radio-gen'>
                                 <input onChange={(e) => handleFilterByGeneration(e)} type="radio" name="generation" value={item} />
@@ -39,7 +45,27 @@ export const SideBar = () => {
                                 </label>
                             </div>
                         ))}
+                    </div>}
+                </div>
+                <div className="sideBar__type">
+                    <div className="sideBar__toggle">
+                        <h3>Тип</h3>
+                        <img
+                            onClick={() => setTypeOpen(!typeOpen)}
+                            className={!typeOpen ? 'open' : 'closed'}
+                            src={arrow}
+                            alt="arrow" />
                     </div>
+                    {typeOpen && <div className={typeOpen ? 'openNav' : 'closedNav'}>
+                        <label className="sideBar__type-checkbox">
+                            <input onChange={() => setPhysicalType(!physicalType)} type="checkbox" />
+                            Физические
+                        </label>
+                        <label className="sideBar__type-checkbox">
+                            <input onChange={() => seSpecialType(!specialType)} type="checkbox" />
+                            Специальные
+                        </label>
+                    </div>}
                 </div>
             </div>
         </aside>
